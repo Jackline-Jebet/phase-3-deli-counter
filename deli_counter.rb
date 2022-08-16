dbc714ec800346253c353c87fce28c89e749b00f
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-16 15:31:38 +0300

    commit tree e3d534ccc6ef5b85e22651da475a5bef56a9b1f4

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(array)
+    if array.empty?
+        puts "The line is currently empty."
+      else
+        arr =  "The line is currently:"
+        array.each.with_index(1) do |person, number|
+          arr << " #{number}. #{person}"
+      end
+        puts arr
+      end
+    
+    end
+    
+    
+    
+    def take_a_number(array, arr)
+        array << arr
+        puts "Welcome, #{arr}. You are number #{array.length} in line."
+    end
+    
+    
+    def now_serving(array)
+        if array.empty?
+            puts "There is nobody waiting to be served!"
+            else
+                puts "Currently serving #{array.first}."
+                array.shift
+        end
+    end
